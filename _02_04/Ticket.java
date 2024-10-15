ca25ed1c1b336e2df1daa125dd2ea5774783e3e1
Natnicha <103370499+natnichalerdwa@users.noreply.github.com>
2024-10-15 18:51:39 +0000

    finished Q2

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -13,7 +13,30 @@ public class Ticket {
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
 
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public void setIsReturn(Boolean isReturn) {
+    this.isReturn = isReturn;
+  }
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 
 }
